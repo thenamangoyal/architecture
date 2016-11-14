5b2e51fb6903da362bf7b49f7cfb82760f79bb6b
Naman Goyal <2015csb1021@iitrpr.ac.in>
2016-11-14 18:58:27 +0530

    Adding extracted dinero

--- cache/d4-7/custom/shellscript.sh
+++ cache/d4-7/custom/shellscript.sh
@@ -13,7 +13,7 @@ do
 			for ASSOCIATIVITY in 1 2 4
 			do
 				echo "Running Dinero for ${MATRIXSIZE}_${CACHESIZE}_${BLOCKSIZE}_${ASSOCIATIVITY}"
-				./dineroIV -l1-dsize ${CACHESIZE} -l1-dbsize ${BLOCKSIZE} -l1-dassoc ${ASSOCIATIVITY} -l1-dccc -informat d < mm_${MATRIXSIZE}.trace > stat_${MATRIXSIZE}_${CACHESIZE}_${BLOCKSIZE}_${ASSOCIATIVITY}.txt
+				../dineroIV -l1-dsize ${CACHESIZE} -l1-dbsize ${BLOCKSIZE} -l1-dassoc ${ASSOCIATIVITY} -l1-dccc -informat d < mm_${MATRIXSIZE}.trace > stat_${MATRIXSIZE}_${CACHESIZE}_${BLOCKSIZE}_${ASSOCIATIVITY}.txt
 			done
 		done
 	done
