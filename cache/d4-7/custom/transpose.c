1dafb0a1b7e9332d649079c074f7947d78e7f971
Naman Goyal <2015csb1021@iitrpr.ac.in>
2016-11-16 19:23:02 +0530

    Added transpose

--- cache/d4-7/custom/transpose.c
+++ cache/d4-7/custom/transpose.c
@@ -21,13 +21,25 @@ int main() {
 		}
 	}
 
+	int T[SIZE][SIZE];
+
+	//Transposing B Matrix into extra memory
+
+	for (i=0; i< SIZE; i++){
+		for (j=0; j < SIZE; j++){
+			T[j][i] = B[i][j];
+			printf("0 %p\n", &B[i][j]);
+			printf("1 %p\n", &T[j][i]);
+		}
+	}
+
 	for(i=0; i<SIZE; i++){
 		for(j=0; j<SIZE; j++){
 			sum = 0;
 			for (k=0; k< SIZE; k++){
-				sum += A[i][k]*B[k][j];
+				sum += A[i][k]*T[j][k];
 				printf("0 %p\n", &A[i][k]);
-				printf("0 %p\n", &B[k][j]);
+				printf("0 %p\n", &T[j][k]);
 			}
 			C[i][j] = sum;
 			printf("1 %p\n", &C[i][j]);
